2849c9aa9942f9f261b6f3ac4268ab511f78c4bb
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2024-06-20 23:27:47 +0200

    uninstall chalk and set manually messagecolors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const chalk = require('chalk');
 require('dotenv').config();
 
 const app = express();
@@ -8,16 +7,20 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const successMessage = (message) => console.log(`\x1b[32m${message}\x1b[0m`);
+const errorMessage = (message) => console.log(`\x1b[31m${message}\x1b[0m`);
+const warningMessage = (message) => console.log(`\x1b[33m${message}\x1b[0m`);
+
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => console.log(chalk.green('MongoDB connected...')))
-  .catch((err) => console.log(chalk.red(`MongoDB connection error: ${err}`)));
+  .then(() => successMessage('MongoDB connected...'))
+  .catch((err) => errorMessage(`MongoDB connection error: ${err}`));
 
 const positionRoutes = require('./routes/positionRoutes');
 app.use('/api/positions', positionRoutes);
 
 app.listen(PORT, () => {
-  console.log(chalk.green(`Server is running on port ${PORT}`));
+  successMessage(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
