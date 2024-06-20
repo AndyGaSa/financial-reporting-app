10796523155f98f34afa5378daa60149812aa755
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2024-06-20 23:22:43 +0200

    install chalk

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const chalk = require('chalk');
 require('dotenv').config();
 
 const app = express();
@@ -8,18 +9,15 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('MongoDB connected...'))
-  .catch((err) => console.log(err));
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log(chalk.green('MongoDB connected...')))
+  .catch((err) => console.log(chalk.red(`MongoDB connection error: ${err}`)));
 
 const positionRoutes = require('./routes/positionRoutes');
 app.use('/api/positions', positionRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(chalk.green(`Server is running on port ${PORT}`));
 });
 
 module.exports = app;
